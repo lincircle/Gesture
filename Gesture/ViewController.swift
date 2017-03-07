fbbfed778da358390975328109e8f7609ab60b79
lincircle <Cosine@Jrtings-MacBook-Pro.local>
2017-03-07 21:06:27 +0800

    commit

--- Gesture/ViewController.swift
+++ Gesture/ViewController.swift
@@ -10,16 +10,91 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var full_size: CGSize!
+    
+    var my_uiview: UIView!
+    
+    var another_uiview: UIView!
+    
+    var circle_uiview: UIView!
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        full_size = UIScreen.main.bounds.size
+        // 底
+        circle_uiview = UIView(frame: CGRect(x: 25, y: full_size.height / 2, width: full_size.width - 50, height: full_size.width - 50))
+        
+        circle_uiview.backgroundColor = UIColor.brown
+        
+        circle_uiview.layer.cornerRadius = circle_uiview.frame.size.width/2
+        
+        circle_uiview.clipsToBounds = true
+        
+        self.view.addSubview(circle_uiview)
+        // 控制器
+        another_uiview = UIView(frame: CGRect(x: full_size.width / 2 - 50, y: full_size.height * 0.75, width: 100, height: 100))
+        
+        another_uiview.backgroundColor = UIColor.red
+        
+        another_uiview.layer.cornerRadius = another_uiview.frame.size.width/2
+        
+        circle_uiview.clipsToBounds = true
+        
+        self.view.addSubview(another_uiview)
+        
+        let pan = UIPanGestureRecognizer(target: self, action: #selector(ViewController.pan))
+        
+        pan.minimumNumberOfTouches = 1
+        
+        pan.maximumNumberOfTouches = 1
+        
+        another_uiview.addGestureRecognizer(pan)
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func pan(recognizer: UIPanGestureRecognizer){
+        
+        switch recognizer.state {
+            
+        case .began :
+            
+            print("began")
+            
+        case .cancelled :
+            
+            print("cancelled")
+            
+        case .changed :
+            
+            print("changed")
+            
+        case .ended :
+            
+            print("ended")
+            
+        case .failed:
+            
+            print("failed")
+            
+        case .possible :
+            
+            print("possible")
+            
+        }
+        
+        let point = recognizer.location(in: self.view)
+        
+        print(point)
+        
+        another_uiview.center = point
+        
+        if recognizer.state == UIGestureRecognizerState.ended {
+            
+            another_uiview.center = CGPoint(x: 300, y: 500)
+            
+        }
+        
     }
-
-
+    
 }
 
